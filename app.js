bbecd04f0c34d6a4826391fa68c2e09b3604dc0e
pluwum <luwyxx@gmail.com>
2018-12-13 23:14:22 +0300

    [Chore]Setup cors middleware.

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const routes = require('./routes/')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+
 const app = express()
 const router = express.Router()
 const port = 3000
 
 // Set up middleware
+app.use(cors())
 app.use(bodyParser.json())
 
 // Set up routes
