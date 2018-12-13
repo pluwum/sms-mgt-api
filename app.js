1c949b6329ded3701f250489e8b3ca7ace4340e1
pluwum <luwyxx@gmail.com>
2018-12-13 23:09:44 +0300

    [Chore]Setup body parser middleware.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const routes = require('./routes/')
-
+const bodyParser = require('body-parser')
 const app = express()
 const router = express.Router()
 const port = 3000
 
+// Set up middleware
+app.use(bodyParser.json())
+
+// Set up routes
 routes(router)
 app.use('/api', router)
 app.listen(port, () => console.log(`server running on ${port}`))
